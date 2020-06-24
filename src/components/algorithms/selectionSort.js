e181775a200796c8cca40044f74b8b80afe04fc7
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-06-25 05:53:35 +0600

    Selection sort

--- src/components/algorithms/selectionSort.js
+++ src/components/algorithms/selectionSort.js
@@ -1,3 +1,40 @@
 export const selectionSortAnimations = (arr) => {
     const animations = []
+    const swap = []
+    for (let i = 0; i < arr.length - 1; i++){
+        let minIndex = i;
+        let oldMinIndex = i;
+        for(let j = i+1; j < arr.length; j++){
+            let obj = {
+                start: i,
+                minIndex,
+                oldMinIndex,
+                iter: j
+            }
+            animations.push(obj)
+            if(arr[j] <= arr[minIndex]){
+                oldMinIndex = minIndex;
+                minIndex = j;
+            }
+            obj = {
+                start: i,
+                minIndex,
+                oldMinIndex,
+                iter: j,
+            }
+            animations.push(obj)
+        }
+        const obj = {
+            start:i,
+            minIndex: minIndex,
+            startVal: arr[i],
+            minVal: arr[minIndex],
+        }
+        arr[i] = arr[minIndex];
+        arr[minIndex] = obj.startVal;
+        animations.push(obj)
+        console.log(animations.length);
+    }
+
+    return {animations,swap};
 }
--- src/components/visualizer/Visualizer.jsx
+++ src/components/visualizer/Visualizer.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './Visualizer.css'
+import { selectionSortAnimations } from '../algorithms/selectionSort';
+
+const ANIMATION_SPEED_MS = 10;
+
 
 const Visualizer = () => {
-    const [arr, setArr] = useState([])
-
+    const [arr, setArr] = useState([]);
     const randomNumberGenerator = (minn, maxx) => {
         return Math.floor(Math.random() * (maxx - minn) + minn);
     }
     const resetArray = () => {
         const arr = [];
-        for(let i = 0; i < 250; i++){
+        for(let i = 0; i < 50; i++){
             arr.push(randomNumberGenerator(5, 800))
         }
         setArr(arr);
@@ -17,21 +20,45 @@ const Visualizer = () => {
 
     const selectionSort = () => {
         const elements = document.getElementsByClassName("array-item");
-        // elements[0].style.backgroundColor = "red"
-        // console.log("to be continued")
-        for(let i = 0; i < elements.length ; i++){
-            setTimeout(() => {
-                elements[i].style.backgroundColor = "red";
-            }, i* 10)
-            // setTimeout(() => {
-            //     elements[i].style.backgroundColor = "rgb(15, 117, 117)";
-            // }, i* 15)
-            // for(let j = 0; j< elements.length; j++){
-            //     setTimeout(() => {
-            //         elements[i].style.backgroundColor = "red"
-            //         elements[j].style.backgroundColor = "red"
-            //     }, i*10000)
-            // }
+        const {animations} = selectionSortAnimations(arr)
+
+        let counter = arr.length * 2 -2;
+        let incr = counter -1 ;
+        let toggler = 0;
+
+        for(let i = 0; i < animations.length ; i++){
+
+            if(i === counter){
+                const {start: startIndex, minIndex, startVal, minVal} = animations[i];
+                setTimeout(() => {
+                    elements[startIndex].style.height = `${minVal}px`
+                    elements[startIndex].style.backgroundColor = `green`
+                    elements[minIndex].style.height = `${startVal}px`
+                    if(minIndex !== startIndex )elements[minIndex].style.backgroundColor = "rgb(15, 117, 117)"
+                    if(startIndex === arr.length-2 )elements[arr.length -1].style.backgroundColor = "green"
+
+                }, i* ANIMATION_SPEED_MS)
+                console.log("pre counter",counter)
+                console.log("post incr",incr)
+                counter+= incr;
+                incr-=2
+                console.log("post counter",counter)
+                console.log("post incr",incr)
+                toggler = toggler ? 0 : 1;
+            }
+            else {
+                const {start, minIndex,iter, oldMinIndex} = animations[i];
+                setTimeout(()=>{
+                    if(start === iter-1)elements[arr.length-1].style.backgroundColor = "rgb(15, 117, 117)";
+                    elements[start].style.backgroundColor = "black";
+                    elements[iter].style.backgroundColor = "red";
+                    if(iter!==0)elements[iter - 1].style.backgroundColor = "rgb(15, 117, 117)";
+                    if(oldMinIndex!==start)elements[oldMinIndex].style.backgroundColor = "rgb(15, 117, 117)";
+                    elements[minIndex].style.backgroundColor = "blue";
+
+                }, i * ANIMATION_SPEED_MS)
+            }
+
         }
     }
     
@@ -52,7 +79,7 @@ const Visualizer = () => {
 
             <div className="d-flex justify-content-center align-items-end flex-wrap array-container">
                 {arr.map((value, idx) => 
-                <div key={idx} className="array-item" style={{height: `${value}px`}}></div>)}
+                <div key={idx} className="array-item" style={{height: `${value}px`, color:`rgb(15, 117, 117)`}}></div>)}
             </div>
         </div>
     );
