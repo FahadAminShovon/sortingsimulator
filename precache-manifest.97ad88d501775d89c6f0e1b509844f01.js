3a8715dca9c03ac12be01e27d1741fb071e0b005
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-06-25 16:10:55 +0600

    Updates

---
+++ precache-manifest.97ad88d501775d89c6f0e1b509844f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29accfb394933b82fae5999c6284785",
+    "url": "/sortingsimulator/index.html"
+  },
+  {
+    "revision": "f8a7f04a432890b70072",
+    "url": "/sortingsimulator/static/css/main.efd91722.chunk.css"
+  },
+  {
+    "revision": "13cf1ffa00ace4d524cc",
+    "url": "/sortingsimulator/static/js/2.2d8f0313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sortingsimulator/static/js/2.2d8f0313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a7f04a432890b70072",
+    "url": "/sortingsimulator/static/js/main.f12f28e4.chunk.js"
+  },
+  {
+    "revision": "bdcbd9749efd1cfd82e7",
+    "url": "/sortingsimulator/static/js/runtime-main.2e1d07f3.js"
+  }
+]);
