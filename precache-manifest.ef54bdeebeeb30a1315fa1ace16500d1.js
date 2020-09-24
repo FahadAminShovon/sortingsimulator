a801ab74693dcbfafb6bcb8c026cfa864c812fd0
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-09-24 06:29:53 +0600

    Updates

---
+++ precache-manifest.ef54bdeebeeb30a1315fa1ace16500d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6247a1fd1438ded9b4a3f135e62a08a8",
+    "url": "/sortingsimulator/index.html"
+  },
+  {
+    "revision": "adb0f15b683115ebecd4",
+    "url": "/sortingsimulator/static/css/main.dd705bb7.chunk.css"
+  },
+  {
+    "revision": "37c5932dd9002c154f40",
+    "url": "/sortingsimulator/static/js/2.4f56f2c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sortingsimulator/static/js/2.4f56f2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb0f15b683115ebecd4",
+    "url": "/sortingsimulator/static/js/main.c4c4c5b1.chunk.js"
+  },
+  {
+    "revision": "bdcbd9749efd1cfd82e7",
+    "url": "/sortingsimulator/static/js/runtime-main.2e1d07f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sortingsimulator/precache-manifest.77c7126a3d46a767b1e27bd4f8e0071c.js"
+  "/sortingsimulator/precache-manifest.ef54bdeebeeb30a1315fa1ace16500d1.js"
 );
 
 self.addEventListener('message', (event) => {
